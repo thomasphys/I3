ae1403a99a2856923029145ee728f66946d41843
Thomas McElroy <tmcelroy@Thomass-MacBook-Air.local>
2020-09-16 13:34:22 -0600

    Added ROOT graphs to the output.

--- domeff/CompilePlotData.py
+++ domeff/CompilePlotData.py
@@ -10,94 +10,89 @@ from datetime import datetime
 from icecube.weighting.fluxes import  Hoerandel5,GaisserH3a
 from icecube.weighting import weighting
 import tables
+import ROOT
 
 opts = {}
 
 # I/O options
 opts["data"] = sys.argv[1]
-opts["nevents"] = int(sys.argv[2])
+opts["eff"] = sys.argv[2]
 opts["out"] = sys.argv[3]
-opts["sim"] = sys.argv[4] == 'true'
 
-DomCharge = np.zeros(40,dtype=float)
-DomCorCharge = np.zeros(40,dtype=float)
-DomCorAttenCharge = np.zeros(40,dtype=float)
+DomCharge_GaisserH4a  = np.zeros(40,dtype=float)
+DomCharge_GaisserH3a  = np.zeros(40,dtype=float)
+DomCharge_GaisserHillas  = np.zeros(40,dtype=float)
+DomCharge_GaisserH4a_IT  = np.zeros(40,dtype=float)
+DomCharge_Hoerandel  = np.zeros(40,dtype=float)
+DomCharge_Hoerandel5  = np.zeros(40,dtype=float)
+DomCharge_Hoerandel_IT  = np.zeros(40,dtype=float)
 
-DomCharge_cor = np.zeros(40,dtype=float)
-DomCorCharge_cor = np.zeros(40,dtype=float)
-DomCorAttenCharge_cor = np.zeros(40,dtype=float)
-
-eventcount = 0.0;
-
+Distances = [0.0,5.0,10.0,15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0,75.0,80.0,85.0,90.0,95.0,100.0,105.0,110.0,115.0,120.0,125.0,130.0,135.0,140.0,145.0,150.0,155.0,160.0,165.0,170.0,175.0,180.0,185.0,190.0,195.0]
 
 ___main___ :
 
 	files_dir = opts["data"]
 	file_list_aux = os.listdir(files_dir)
-	file_list = [x for x in file_list_aux if '.h5' in x]
+	file_list_h5 = [x for x in file_list_aux if '.h5' in x]
+	file_list_h5 = [x for x in file_list_h5 if opts["eff"] in x]
 
-	gen = weighting.from_simprod(simprod_number) #per file generator simprod_number = 12359
+	fout = ROOT.TFile(opts["out"]," RECREATE ")
 
-    gen = gen*len(file_list)
-
-    per_event_weights = flux(primary_energies,primary_types)/gen(primary_energies,primary_types)
+	eventcount = 0
 	
 	for filename in file_list :
 		h5file = h5py.File(filename, 'r')
 
 		domtable = h5file.root.doms
+		eventtable = h5file.root.events
 
-		primaryenergy = np.array(h5file['Events'][:].primary.energy)
-		primarytype = np.array(h5file['Events'][:].primary.id)
+		eventcount += eventtable.nrows()
+		domindex = 0
 
-		flux = GaisserH3a()
-		eventweight = np.zeros(len(primaryenergy),dtype=float)
-		eventweight = eventweight + 1.0
-		if primarytype[0] >= 0 :
-			eventweight = flux(primaryenergy,primarytype)/gen(primaryenergies,primarytypes)
+		for event in eventtable.iterrows() :
+			DomCharge_event = np.zeros(40,dtype=float)
+			DomCharge_norm = np.zeros(40,dtype=float)
+			for dom in domtable.iterrows(domindex) :
+				if  event['id'] != dom['eventId'] :
+					break
+				domindex += 1
 
-		DomCorCharge_event = np.zeros(40,dtype=float)
-		DomCorAttenCharge_event = np.zeros(40,dtype=float)
-		DomCharge_event = np.zeros(40,dtype=float)
-		DomCorCharge_norm = np.zeros(40,dtype=float)
-		DomCorAttenCharge_norm = np.zeros(40,dtype=float)
-		DomCharge_norm = np.zeros(40,dtype=float)
+				dist = dom['recoDist']
+				char = dom['totalCharge']
+				i_dist = (int)(dist/5.0)
+				if i_dist > 0 and i_dist < 40 :
+					DomCharge_event[i] += char
+					DomCharge_norm[i] += 1.0
 
-		i_event = 0
+			for j in range(len(DomCorCharge_norm)) :
+				if DomCharge_norm[j] > 0.0 :
+					DomCharge_GaisserH4a[j] += event['weight_GaisserH4a']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_GaisserH3a[j] += event['weight_GaisserH3a']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_GaisserH4a_IT[j] += event['weight_GaisserH4a_IT']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_Hoerandel[j] += event['weight_Hoerandel']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_Hoerandel5[j] += event['weight_Hoerandel5']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_Hoerandel_IT[j] += event['weight_Hoerandel_IT']*DomCharge_event[j]/DomCharge_norm[j]
+					DomCharge_GaisserHillas[j] += event['weight_GaisserHillas']*DomCharge_event[j]/DomCharge_norm[j]
+					
 
-		for dom in domtable.iterrows() :
-			if i_event != dom['eventId']
-				eventcount += 1.0
-				for j in range(len(DomCorCharge_norm)) :
-					if DomCorCharge_norm[j] > 0.0 :
-						DomCorCharge[j] += eventweight[i_event]*DomCorCharge_event[j]/DomCorCharge_norm[j]
-					if DomCorAttenCharge_norm[j] > 0.0 :
-						DomCorAttenCharge[j] += eventweight[i_event]*DomCorAttenCharge_event[j]/DomCorAttenCharge_norm[j]
-					if DomCharge_norm[j] > 0.0 :
-						DomCharge[j] += eventweight[i_event]*DomCharge_event[j]/DomCharge_norm[j]
-				i_event = dom['eventId']
-				DomCorCharge_norm = np.zeros(40,dtype=float)
-				DomCorAttenCharge_norm = np.zeros(40,dtype=float)
-				DomCorCharge_event = np.zeros(40,dtype=float)
-				DomCorAttenCharge_event = np.zeros(40,dtype=float)
-				DomCharge_event = np.zeros(40,dtype=float)
-				DomCharge_norm = np.zeros(40,dtype=float)
-			
-			dist = dom['recoDist']
-			char = dom['totalCharge']
-			CorChar = correctedcharge(dist,char)
-			CorAttenChar = correctedAttencharge(dist,char)
-			i_dist = (int)(dist/5.0)
-			if i_dist > 0 and i_dist < 40 :
-				DomCharge_event[i] += char
-				DomCorCharge_event[i] += CorChar
-				DomCorAttenCharge_event[i] += CorAttenChar
-				DomCharge[i] += char
-				DomCorCharge[i] += CorChar
-				DomCorAttenCharge[i] += CorAttenChar
-				DomCharge_norm[i] += 1.0
+	print(DomCharge)
+	tg_GaisserH4a = ROOT.TGraph(len(Distance),Distance,DomCharge_GaisserH4a)
+	tg_GaisserH3a = ROOT.TGraph(len(Distance),Distance,DomCharge_GaisserH3a)
+	tg_GaisserH4a_IT = ROOT.TGraph(len(Distance),Distance,DomCharge_GaisserH4a_IT)
+	tg_Hoerandel = ROOT.TGraph(len(Distance),Distance,DomCharge_Hoerandel)
+	tg_Hoerandel5 = ROOT.TGraph(len(Distance),Distance,DomCharge_Hoerandel5)
+	tg_Hoerandel_IT = ROOT.TGraph(len(Distance),Distance,DomCharge_Hoerandel_IT)
+	tg_GaisserHillas = ROOT.TGraph(len(Distance),Distance,DomCharge_GaisserHillas)
 
-		print(DomCharge)
 
+	fout.cd()
+	tg_GaisserH4a.Write("GaisserH4a")
+	tg_GaisserH3a.Write("GaisserH3a")
+	tg_GaisserH4a_IT.Write("GaisserH4a_IT")
+	tg_Hoerandel.Write("Hoerandel")
+	tg_Hoerandel5.Write("Hoerandel5")
+	tg_Hoerandel_IT.Write("Hoerandel_IT")
+	tg_GaisserHillas.Write("GaisserHillas")
+	fout.Close()
 
 
